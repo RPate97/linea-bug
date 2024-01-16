d706be90afeee7173de62990a9b561a896b3de09
Ryan Pate <ryantpate97@gmail.com>
2024-01-16 11:44:56 -0800

    a

---
+++ script/HelloSphinx.s.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { HelloSphinx } from "../src/HelloSphinx.sol";
+import { Script } from "forge-std/Script.sol";
+import "@sphinx-labs/plugins/SphinxPlugin.sol";
+
+contract HelloSphinxScript is Script, Sphinx {
+    HelloSphinx helloSphinx;
+
+    function setUp() public virtual {
+        sphinxConfig.owners = [0x9fd58Bf0F2E6125Ffb0CBFa9AE91893Dbc1D5c51];
+        sphinxConfig.orgId = "clo6byksj0001cbld6lelntej";
+        sphinxConfig.threshold = 1;
+        sphinxConfig.projectName = "My_Demo_Project";
+        sphinxConfig.testnets = [
+            Network.sepolia,
+            Network.optimism_sepolia,
+            Network.arbitrum_sepolia,
+            Network.base_sepolia,
+            Network.linea_goerli,
+            Network.polygon_zkevm_goerli,
+            Network.fantom_testnet,
+            Network.polygon_mumbai,
+            Network.bnb_testnet,
+            Network.avalanche_fuji,
+            Network.gnosis_chiado
+        ];
+    }
+
+    function run() public sphinx {
+        helloSphinx = new HelloSphinx{ salt: bytes32(0) }("Hi", 2);
+        helloSphinx.add(8);
+    }
+}
