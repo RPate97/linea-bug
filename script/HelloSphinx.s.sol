55c62fa34b2b4f2e5138c11c981cf786deca1b5c
Ryan Pate <ryantpate97@gmail.com>
2024-01-17 14:45:16 -0800

    a

--- script/HelloSphinx.s.sol
+++ script/HelloSphinx.s.sol
@@ -9,23 +9,24 @@ contract HelloSphinxScript is Script, Sphinx {
     HelloSphinx helloSphinx;
 
     function setUp() public virtual {
-        sphinxConfig.owners = [0xFBD0C9AA5356577F09e8cA7681A88cC8C9DF52F0];
-        sphinxConfig.orgId = "clkskjg9t0000zjcb1lri0nvr";
+        sphinxConfig.owners = [0x9fd58Bf0F2E6125Ffb0CBFa9AE91893Dbc1D5c51];
+        sphinxConfig.orgId = "clo6byksj0001cbld6lelntej";
         sphinxConfig.threshold = 1;
         sphinxConfig.projectName = "My_Demo_Project";
         sphinxConfig.testnets = [
-            Network.sepolia,
-            Network.optimism_sepolia,
-            Network.arbitrum_sepolia,
-            Network.base_sepolia,
-            Network.polygon_zkevm_goerli,
-            Network.fantom_testnet,
-            Network.polygon_mumbai,
-            Network.bnb_testnet,
-            Network.avalanche_fuji,
-            Network.gnosis_chiado
+          Network.linea_goerli
+            // Network.sepolia,
+            // Network.optimism_sepolia,
+            // Network.arbitrum_sepolia,
+            // Network.base_sepolia,
+            // Network.polygon_zkevm_goerli,
+            // Network.fantom_testnet,
+            // Network.polygon_mumbai,
+            // Network.bnb_testnet,
+            // Network.avalanche_fuji,
+            // Network.gnosis_chiado
         ];
-        sphinxConfig.saltNonce = 116242;
+        sphinxConfig.saltNonce = 11724120;
     }
 
     function run() public sphinx {
